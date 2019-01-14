9a80f0e39cf3be24bfae9274f2e1dc7712445c1e
SeoHyeonMyeong <gusaud2199@naver.com>
2019-01-14 22:53:07 +0900

    Make Canvas and Show It.

---
+++ canvas.js
@@ -0,0 +1,14 @@
+﻿function CanvasManager() {
+	this.canvas = document.querySelector(".my-canvas");
+	this.canvasCtx = this.canvas.getContext('2d');
+	this.setup();
+}
+
+CanvasManager.prototype.setup = function() {	// 캔버스를 흰색으로 색칠
+	this.canvasCtx.fillStyle = "#fff";
+	this.canvasCtx.fillRect(0,0,500,500);
+}
+
+document.addEventListener("DOMContentLoaded",function() {
+	var manager = new CanvasManager();
+});
