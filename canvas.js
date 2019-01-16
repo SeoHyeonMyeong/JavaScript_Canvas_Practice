d73df27b6579cf1b37f34bd5340ed0c31922ce9c
SeoHyeonMyeong <gusaud2199@naver.com>
2019-01-16 23:42:25 +0900

    Add Image and Animation Code to Canvas

--- canvas.js
+++ canvas.js
@@ -11,6 +11,7 @@ function CanvasManager(canvas) {
 	this.drawRect();
 	this.drawTriangle();
 	this.drawCircle();
+	this.drawImage();
 }
 
 CanvasManager.prototype.setup = function() {	// 캔버스를 흰색으로 색칠
@@ -41,12 +42,34 @@ CanvasManager.prototype.drawCircle = function() {	// 원 그리기
 	this.canvasCtx.fill();
 }
 
+CanvasManager.prototype.drawImage = function() {	// 이미지 그리기
+	var self = this;
+	var img = new Image();
+	img.addEventListener("load",function(){
+		self.canvasCtx.drawImage(img,300,10,100,100);
+	},false);
+	img.src = 'images/Stitch.jpg';
+}
+// Rectangle.js
+
+var input = {
+	up : false,
+	down : false,
+	right : false,
+	left : false,
+	quit : false,
+};
+
+var updateInterval;
+
 function Rectangle(canvas,x,y) {
+	var self = this;
 	this.canvas = canvas;
 	this.canvasCtx = this.canvas.getContext('2d');
 	this.x = x;
 	this.y = y;
 	this.sticks=0;
+	
 	this.addKeyDownEvent();
 	this.draw();
 }
@@ -54,54 +77,67 @@ function Rectangle(canvas,x,y) {
 Rectangle.prototype.addKeyDownEvent = function() {
 	var self = this;
 	document.addEventListener("keydown",function(e) {
-		if(e.key==="ArrowUp"&&self.y>=30){
-			self.hop();
-			self.draw();
+		if(e.key==="ArrowUp"){
+			input.up = true;
 		}
-		if(e.key==="ArrowDown"&&self.y<=470){
-			self.fall();
-			self.draw();
+		if(e.key==="ArrowDown"){
+			input.down = true;
 		}
-		if(e.key==="ArrowLeft"&&self.x>=30){
-			self.moveLeft();
-			self.draw();
+		if(e.key==="ArrowLeft"){
+			input.left = true;
 		}
-		if(e.key==="ArrowRight"&&self.x<=470){
-			self.moveRight();
-			self.draw();
+		if(e.key==="ArrowRight"){
+			input.right = true;
+		}
+		if(e.key==="q"){
+			input.quit = true;
+		}
+		self.update();
+	});	
+	document.addEventListener("keyup",function(e) {
+		if(e.key==="ArrowUp"){
+			input.up = false;
+		}
+		if(e.key==="ArrowDown"){
+			input.down = false;
+		}
+		if(e.key==="ArrowLeft"){
+			input.left = false;
+		}
+		if(e.key==="ArrowRight"){
+			input.right = false;
 		}
 	});
+}
+
+
+
+Rectangle.prototype.draw = function() {
+	var self = this;
+	this.canvasCtx.fillStyle = "rgba(255,255,255,1)";
+	this.canvasCtx.fillRect(0,0,500,500);
+	if(input.up&&this.y>=14) this.y-=4;
+	if(input.down&&this.y<=386) this.y+=4;
+	if(input.right&&this.x<=386) this.x+=4;
+	if(input.left&&this.x>=14) this.x-=4;
+	var img = new Image();
+	img.src = 'images/Stitch.jpg';
+	self.canvasCtx.drawImage(img,self.x,self.y,100,100);
 	
 }
 
-Rectangle.prototype.hop = function() {
-	this.y -= 10;
+Rectangle.prototype.update = function() {
+	var self = this;
+	if(input.quit) {
+		window.clearInterval(updateInterval);
+	}
 }
-
-Rectangle.prototype.fall = function() {
-	this.y += 10;
-}
-
-Rectangle.prototype.moveLeft = function() {
-	this.x -= 10;
-}
-
-Rectangle.prototype.moveRight = function() {
-	this.x += 10;
-}
-
-Rectangle.prototype.draw = function() {
-	this.canvasCtx.save();
-	var image = new Path2D('M'+this.x +' '+this.y+' h 80 v -80 Z');
-	this.canvasCtx.fillStyle = "rgba(100,200,200,1)";
-	this.canvasCtx.fill(image);
-	this.canvasCtx.restore();
-}
-
 
 // application.js
+var rectangle;
 document.addEventListener("DOMContentLoaded",function() {	// 로드시 이벤트
 	var canvas = document.querySelector('.my-canvas');
 	var manager = new CanvasManager(canvas);
-	var rectangle = new Rectangle(canvas,20,480);
+	rectangle = new Rectangle(canvas,10,10);
+	updateInterval = window.setInterval("rectangle.draw()",30);	
 });
